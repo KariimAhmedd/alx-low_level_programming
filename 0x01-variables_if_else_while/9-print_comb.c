dd12695c95334047a95ac344f232f1c3f28271ca
KariimAhmedd Hub <karim2elsaid@gmail.com>
2023-03-19 17:05:28 -0700

    '

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+		putchar(j + '0');
+
+			if (i != 8 || j != 9)
+		{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
